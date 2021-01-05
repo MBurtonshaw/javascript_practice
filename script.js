e425619939f52b356c5b9a0b360dbda2a9f424f9
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-01-05 10:55:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-app.use(cookieParser);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.set("view engine", "pug");
 
+//ROOT///////////////////////////////////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
     res.render("index");
 });
-
-/*app.get("/list", (req, res) => {
-    res.render("list");
+//For the following, "name" must be stored as a local variable to be accessed
+app.post("/", (req, res) => {
+    res.render("index", { name: req.cookies.username });
 });
 
+//LIST///////////////////////////////////////////////////////////////////////////////////////////
+app.get("/list", (req, res) => {
+    res.render("list", { name: req.cookies.username });
+});
+//The redirect allows the cookie to be stored
 app.post("/list", (req, res) => {
-    res.render("list"); //, {name: res.cookies.username});
-});*/
+    res.cookie("username", req.body.username);
+    res.redirect("list");
+});
 
+//SERVER////////////////////////////////////////////////////////////////////////////////////////////
 app.listen(3000, () => {
     console.log("working");
 });
+
+//////////////////////////////////////////////////////////NEXT///////////////////////////////////////////////////////////////////////////////////
+//Redirect users to the root if there is no username entered. Don't allow passage without a username
+//Once username is created, make two buttons on redirect page. One to change username and the other to enter the List
+//Begin List
