26e37220481fd69c6b45257bd8f64b7f5cf1a1da
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-01-08 19:25:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,46 +2,70 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const app = express();
-const list = [];
+class Person {
+    constructor(id, name, item) {
+        this.id = id;
+        this.name = name;
+        this.item = item;
+    }
+};
+const personList = [];
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use("/static", express.static("public"));
+
 app.set("view engine", "pug");
 
-//ROOT///////////////////////////////////////////////////////////////////////////////////////////
 app.get("/", (req, res) => {
-    res.render("index");
-});
-//For the following, "name" must be stored as a local variable to be accessed
-app.post("/", (req, res) => {
-    res.render("index", { name: req.cookies.username });
+    res.render('layout');
 });
 
-//LIST///////////////////////////////////////////////////////////////////////////////////////////
-app.get("/list", (req, res) => {
-    res.render("list", { name: req.cookies.username, list_item: req.cookies.list_item });
-});
-//The redirect allows the cookie to be stored
-app.post("/list", (req, res) => {
+app.post("/", (req, res) => {
     res.cookie("username", req.body.username);
+    res.redirect("/item");
+});
+
+app.get("/item", (req, res) => {
+    const name = req.cookies.username;
+    if (name) {
+        res.render("item");
+    } else {
+        res.redirect("/");
+    }
+});
+
+app.post("/item", (req, res) => {
+    res.cookie("list_item", req.body.list_item);
     res.redirect("/list");
 });
 
-app.get("/login", (req, res) => {
-    res.render("login", { name: req.cookies.username, list_item: req.cookies.list_item});
+app.get("/list", (req, res) => {
+    const item = req.cookies.list_item;
+    if (item) {
+        let newPerson = new Person(personList.length, req.cookies.username, req.cookies.list_item);
+        personList.push(newPerson);
+        res.render('list');
+        console.log(personList);
+    } else {
+        res.redirect('/item');
+    }
 });
 
-app.post("/login", (req, res) => {
-    res.cookie("list_item", req.body.list_item);
-    res.redirect("/login");
+////ERROR MESSAGE////
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-//SERVER////////////////////////////////////////////////////////////////////////////////////////////
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.send(JSON.stringify({ok: false, message: err.message}));
+});
+
+////SERVER////
 app.listen(3000, () => {
     console.log("working");
 });
-
-//////////////////////////////////////////////////////////NEXT///////////////////////////////////////////////////////////////////////////////////
-//Redirect users to the root if there is no username entered. Don't allow passage without a username
-//Once username is created, make two buttons on redirect page. One to change username and the other to enter the List
-//Begin List
